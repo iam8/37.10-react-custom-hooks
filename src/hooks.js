15e479930f7c7864dab6bc4f8c38dca78bbfb988
alexm <alex-m@outlook.com>
2023-07-26 18:15:24 -0700

    Write useAxios() custom hook and documentation

--- src/hooks.js
+++ src/hooks.js
@@ -1,12 +1,17 @@
 import {useState} from "react";
-
+import axios from "axios";
+import { v4 as uuid } from "uuid";
 
 
 /**
- * Custom hook - toggle a state (true -> false, false -> true).
+ * Toggle a state (true -> false, false -> true).
+ *
+ * Returns:
+ * - Array of 2 elements: state (initially true), and a function to toggle this state.
  */
 function useFlip() {
     const [state, setState] = useState(true);
+
     const flipState = () => {
         setState(state => !state);
     }
@@ -15,8 +20,34 @@ function useFlip() {
 }
 
 
-function useAxios() {
+/**
+ * Make an Axios GET request to the given URL and add the response data object to an array in
+ * state.
+ *
+ * Generate and add a new unique ID with each new response data object.
+ *
+ * Pop up an alert and add nothing to array if error occurs on Axios request.
+ *
+ * Returns:
+ * - Array of 2 elements: state (initially empty array), and a function to make Axios request and
+ *      add the response data to the state array.
+ */
+async function useAxios(url) {
+    const [array, setArray] = useState([]);
 
+    const setDataArray = async () => {
+        let result;
+        try {
+            result = await axios.get(url);
+        } catch(err) {
+            alert(err);
+            return;
+        }
+
+        setArray(array => [...array, {...result.data, id: uuid()}]);
+    }
+
+    return [array, setDataArray];
 }
 
 
