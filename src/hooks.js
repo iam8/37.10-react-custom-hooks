8e6a6b78dce4e661523046d38cc5b8db2b41dec1
alexm <alex-m@outlook.com>
2023-07-26 17:41:43 -0700

    Write custom hook for flipping (toggling) state

--- src/hooks.js
+++ src/hooks.js
@@ -3,6 +3,12 @@ import React, {useState} from "react";
 
 function useFlip() {
 
+    const [state, setState] = useState(true);
+    const flipState = () => {
+        setState(state => !state);
+    }
+
+    return [state, flipState];
 }
 
 
