109820e57085741b9c180deaf9474545056423ad
alexm <alex-m@outlook.com>
2023-07-26 17:42:15 -0700

    Import and use useFlip custom hook in component

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
+import { useFlip } from "./hooks";
 import backOfCard from "./back.png";
 import "./PlayingCard.css"
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
-  return (
-    <img
-      src={isFacingUp ? front : back}
-      alt="playing card"
-      onClick={flipCard}
-      className="PlayingCard Card"
-    />
-  );
+    const [isFacingUp, toggleIsFacingUp] = useFlip();
+
+    return (
+        <img
+            src={isFacingUp ? front : back}
+            alt="playing card"
+            onClick={toggleIsFacingUp}
+            className="PlayingCard Card"
+        />
+    );
 }
 
 export default PlayingCard;
