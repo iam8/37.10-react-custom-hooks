6808cc5ab1e6f24dec0ad1fba7328af8296fc0d9
alexm <alex-m@outlook.com>
2023-07-26 18:15:01 -0700

    Adjust indentation

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -7,26 +7,27 @@ import "./PlayingCardList.css";
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function PlayingCardList() {
-  const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
+    const [cards, setCards] = useState([]);
+    const addCard = async () => {
+        const response = await axios.get(
+        "https://deckofcardsapi.com/api/deck/new/draw/"
+        );
+
+        setCards(cards => [...cards, { ...response.data, id: uuid() }]);
+    };
+    return (
+        <div className="PlayingCardList">
+        <h3>Pick a card, any card!</h3>
+        <div>
+            <button onClick={addCard}>Add a playing card!</button>
+        </div>
+        <div className="PlayingCardList-card-area">
+            {cards.map(cardData => (
+            <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
+            ))}
+        </div>
+        </div>
     );
-    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
-  };
-  return (
-    <div className="PlayingCardList">
-      <h3>Pick a card, any card!</h3>
-      <div>
-        <button onClick={addCard}>Add a playing card!</button>
-      </div>
-      <div className="PlayingCardList-card-area">
-        {cards.map(cardData => (
-          <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
-        ))}
-      </div>
-    </div>
-  );
 }
 
 PlayingCardList.defaultProps = {};
