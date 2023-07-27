521af62e2153b53b00bfa11ecb1e28c1758432d9
alexm <alex-m@outlook.com>
2023-07-26 19:52:45 -0700

    Pass in base URL to useAxios() and provide rest of URL in event handler for 'add card' button click

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -3,17 +3,19 @@ import { useAxios } from "./hooks";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
 
+const BASE_URL = "https://deckofcardsapi.com/api";
+
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function PlayingCardList() {
-    const [cards, addCard] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/");
+    const [cards, addCard] = useAxios(BASE_URL);
 
     return (
         <div className="PlayingCardList">
             <h3>Pick a card, any card!</h3>
             <div>
-                <button onClick={addCard}>Add a playing card!</button>
+                <button onClick={() => addCard("/deck/new/draw/")}>Add a playing card!</button>
             </div>
             <div className="PlayingCardList-card-area">
                 {cards.map(cardData => (
